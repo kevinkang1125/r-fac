556f025bf7810f310c57878d7ad0916bfbb30f26
kevinkang <e0751516@u.nus.edu>
2023-09-19 00:40:52 +0800

    code update

--- VDN/multi_robot_utils_off_policy.py
+++ VDN/multi_robot_utils_off_policy.py
@@ -3,7 +3,7 @@ from tqdm import tqdm
 import numpy as np
 import torch
 import collections
-import random
+import random                           
 import math
 
 def train_off_policy_multi_agent(env, agents, replay_buffers, num_episodes, minimal_size, batch_size):
@@ -24,6 +24,7 @@ def train_off_policy_multi_agent(env, agents, replay_buffers, num_episodes, mini
 ##pre-deployment for prototype
 def train_V2DN_off_policy_multi_agent(env, mixer, agents, replay_buffers, num_episodes, batch_size, rho):
     return_multi_list = []
+    td_list = []
     epoch_num = 10
     for i in range(epoch_num):
         with tqdm(total=int(num_episodes / epoch_num), desc='Iteration %d' % i) as pbar:
@@ -31,12 +32,13 @@ def train_V2DN_off_policy_multi_agent(env, mixer, agents, replay_buffers, num_ep
                 episode_return,alive_index = each_sampling(env, agents, replay_buffers,rho)
                 return_multi_list.append(episode_return)
                 #if replay_buffers[0].size() > minimal_size:
-                central_train_off_policy(mixer, agents, replay_buffers,batch_size,alive_index)
+                td_error = central_train_off_policy(mixer, agents, replay_buffers,batch_size,alive_index)
+                td_list.append(td_error)
                 if (i_episode + 1) % 10 == 0:
                     pbar.set_postfix({'episode': '%d' % (num_episodes / epoch_num * i + i_episode + 1),
                                       'return': '%.3f' % np.mean(return_multi_list[-10:])})
                 pbar.update(1)
-    return return_multi_list
+    return return_multi_list,td_list
 
 def train_V2DN_on_policy_multi_agent(env, mixer, agents, replay_buffers, num_episodes, batch_size, rho):
     return_multi_list = []
@@ -127,5 +129,6 @@ def central_train_off_policy(mixer,agents,replay_buffers,batch_size,alive_index)
         current_q_values[:,i:i+1],next_max_q_values[:,i:i+1],rewards[:,i:i+1] = agent.output_agent(transition_dict)
         tran_list.append(transition_dict)
 
-    mixer.learn(current_q_values, next_max_q_values, rewards, alive_index,tran_list)   
+    td_error = mixer.learn(current_q_values, next_max_q_values, rewards, alive_index,tran_list)
+    return td_error.item()   
     
