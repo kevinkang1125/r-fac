70792cb9e8f6112446bbe0fbe6cb7aeaf20f1321
KevinKang <e0751516@u.nus.edu>
2023-09-21 20:03:35 +0800

    test

--- VDN/try_python.py
+++ VDN/try_python.py
@@ -1,13 +1,23 @@
 import math
 import numpy as np
 import torch
-a = [1,2,3]
-b = []
-b.append(a)
-b.append(a)
-print(len(a),len(b[0]))
-c = np.exp(a)
+a = torch.tensor([1,2,3])
+b = torch.tensor([1,2])
+c = torch.zeros([1,3])
+c[:,0:len(b)] = b
+#c = a + b
 print(c)
-a = torch.tensor(a)
-a = a.pow(2)
-print(a)
+transition_dicts = [{'observations': [], 'actions': [], 'next_states': [], 'next_observations': [], 'rewards': [], 'rewards_part2': [],
+                        'dones': [], 'action_num': []} for _ in range(5)]
+print(len(transition_dicts[2]["rewards"]))
+# b = []
+# b.append(a)
+# b.append(a)
+# print(len(a),len(b[0]))
+# c = np.exp(a)
+# print(c)
+# a = torch.tensor(a)
+# a = a.pow(2)
+# print(a)
+# a = range(10)
+# print(a)
