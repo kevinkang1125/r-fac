59b6b97c37c5d60e59460e33a48ae3b396a9c3bb
kevinkang <e0751516@u.nus.edu>
2023-09-21 23:21:24 +0800

    log

--- VDN/try_python.py
+++ VDN/try_python.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 import torch
+import random
 a = torch.tensor([1,2,3])
 b = torch.tensor([1,2])
 c = torch.zeros([1,3])
@@ -10,6 +11,22 @@ print(c)
 transition_dicts = [{'observations': [], 'actions': [], 'next_states': [], 'next_observations': [], 'rewards': [], 'rewards_part2': [],
                         'dones': [], 'action_num': []} for _ in range(5)]
 print(len(transition_dicts[2]["rewards"]))
+counter = 0
+rho_list = [2,5,7]
+alive_index = [0,1,2]
+alive_list = []
+while counter < 10:
+    if counter in rho_list:
+        robot = random.choice(alive_index)
+        alive_index.remove(robot)
+    print(counter,alive_index)
+    alive_list.append(alive_index.copy())
+    counter +=1
+
+    print(alive_list)
+   
+
+
 # b = []
 # b.append(a)
 # b.append(a)
