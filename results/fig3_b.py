1421306b530901b380adcd5e9f0cd642e068c424
KevinKang <e0751516@u.nus.edu>
2023-10-18 18:15:56 +0800

    finish resiliency calculation

--- results/fig3_b.py
+++ results/fig3_b.py
@@ -14,29 +14,45 @@ plt.rcParams["axes.labelweight"] = "bold"
 plt.rcParams["axes.linewidth"] = 2
 
 
-w_NC_mean = [50.884,38.445,34.45]
-PatrolGRAPHstar_mean = [56.43,52.29,46.157]
-PatrolGRAPHA_mean = [53.455,34.689,30.71]
-SCTSP_mean = [53,35.987,36.37]
-DQN_mean = [58.996,50.854,31.8]
-EMP_mean = [33.755,	29.71,22.68]
+tick_label = ['3', '4', '5']
+#museum pre resiliency score
+V2DN = [64.98 ,62.06, 58.192]
+CE_PG = [280.28,231.18,224.57]
+MADDPG = [142.977,121.27,116.71]
+VDN = [140.53,131.41,125.93]
+DQN = [158.95, 149.211, 140.86]
+DRL = [219.45,202.68,181.84]
+PD_Fac = [149.14,136.47,128.57]
+MASAC = [196.9,189.23,177.43]
+#office pre resilency score
+V2DN = [141.11,130.92,122.95]
+CE_PG = [317.35,310.75,301.65]
+MADDPG = [281.82,214.08,202.21]
+VDN = [278.57,199.89,198.63]
+DQN = [263.45,282.71,231.05]
+DRL = [311.4,282.11,234.74]
+PD_Fac = [255.14,237.75,226.53]
+MASAC = [285.93,278.5,274.84]
 
-w_NC_mean = [54.38,42.366,36]
-PatrolGRAPHstar_mean = [82.056,56.3,45.44]
-PatrolGRAPHA_mean = [46.57,36.617,31.34]
-SCTSP_mean = [50.513,39.34,37.07]
-DQN_mean = [63.89,54.456,47.4]
-EMP_mean = [36.296,	26.551,24.174]
-bar_width = 0.1
-bar_widthg = 0.105
-tick_label = ['2', '3', '4']
+#museum during resiliency score
+V2DN = [121.75,142.36,190]
+CE_PG = [286.76,308.57,426.7]
+MADDPG = [258.12,248.79,325]
+VDN = [201.23,251.64,312.3]
+DQN = [298.23,318.79,345.8]
+DRL = [238.23,281.57,390]
+PD_Fac = [233.53,208.96,282.8]
+MASAC = [275.65,308.86,403.2]
 
-V2DN = [39,34.74,25.968]
-CE_PG = [56.5,54.76,37.09]
-MADDPG = [55.029,38.33,34.079]
-VDN = [53.23,40.3,30.307]
-DQN = [70.06,44.84,37.3]
-DRL = [56.505,50,45.025]
+#office during resilency score
+V2DN = [198.08,244.63,332.8]
+CE_PG = [370.83,476.42,518.17]
+MADDPG = [358.56,303.47,467.98]
+VDN = [343.58,324.21,405.11]
+DQN = [483.83,372,521.67]
+DRL = [370.86,426.32,650.42]
+PD_Fac = [385,326.32,433.33]
+MASAC = [381,454.58,569.08]
 
 # V2DN = [37.7,33.93,29]
 # CE_PG = [65.75,57.2,52.67]
@@ -46,18 +62,20 @@ DRL = [56.505,50,45.025]
 # DRL = [57.5,53.42,49]
 bar_width = 0.1
 bar_widthg = 0.105
-tick_label = ['3', '4', '5']
+tick_label = ['2', '3', '4']
 
-plt.bar(np.arange(3), V2DN, bar_width, align="center", label="V2DN", alpha=1,ec='k', ls='-', lw=3,color=(78/255, 171/255, 144/255))
-plt.bar(np.arange(3)+bar_widthg, CE_PG, bar_width, align="center", label="CE-PG", alpha=1,ec='k', ls='-', lw=3,color=(142/255, 182/255, 156/255))
-plt.bar(np.arange(3)+2*bar_widthg, MADDPG, bar_width, align="center", label="MADDPG", alpha=1,ec='k', ls='-', lw=3,color=(237/255, 221/255, 195/255))
-plt.bar(np.arange(3)+3*bar_widthg, VDN, bar_width, align="center", label="VDN", alpha=1,ec='k', ls='-', lw=3,color=(238/255, 191/255, 109/255))
-plt.bar(np.arange(3)+4*bar_widthg, DQN, bar_width, align="center", label="DQN", alpha=1,ec='k', ls='-', lw=3,color=(217/255, 79/255, 51/255))
-plt.bar(np.arange(3)+5*bar_widthg, DRL, bar_width, align="center", label="DRL", alpha=1,ec='k', ls='-', lw=3,color=(131/255, 64/255, 38/255))
+plt.bar(np.arange(3), V2DN, bar_width, align="center", label="V2DN", alpha=1,ec='k', ls='-', lw=3,color=(163/255, 6/255, 67/255))
+plt.bar(np.arange(3)+bar_widthg, CE_PG, bar_width, align="center", label="CE-PG", alpha=1,ec='k', ls='-', lw=3,color=(235/255, 96/255, 70/255))
+plt.bar(np.arange(3)+2*bar_widthg, MADDPG, bar_width, align="center", label="MADDPG", alpha=1,ec='k', ls='-', lw=3,color=(98/255, 190/255, 166/255))
+plt.bar(np.arange(3)+3*bar_widthg, VDN, bar_width, align="center", label="VDN", alpha=1,ec='k', ls='-', lw=3,color=(254/255, 251/255, 185/255))
+plt.bar(np.arange(3)+4*bar_widthg, DQN, bar_width, align="center", label="DQN", alpha=1,ec='k', ls='-', lw=3,color=(205/255, 234/255, 157/255))
+plt.bar(np.arange(3)+5*bar_widthg, DRL, bar_width, align="center", label="DRL", alpha=1,ec='k', ls='-', lw=3,color=(253/255, 186/255, 107/255))
+plt.bar(np.arange(3)+6*bar_widthg, PD_Fac, bar_width, align="center", label="PD-FAC", alpha=1,ec='k', ls='-', lw=3,color=(236/255, 217/255, 207/255))
+plt.bar(np.arange(3)+7*bar_widthg, MASAC, bar_width, align="center", label="MASAC", alpha=1,ec='k', ls='-', lw=3,color=(78/255, 146/255, 128/255))
 
-plt.ylim(20,120)
+plt.ylim(150,800)
 plt.xlabel("Number of Robots ($N$)", weight='bold',fontsize=40)
-plt.ylabel("Capture Time", weight='bold',fontsize=40)
+plt.ylabel("Resiliency Score", weight='bold',fontsize=40)
 plt.xticks(weight='bold')
 plt.yticks(weight='bold')
 plt.xticks(np.arange(3)+bar_widthg*2.5,tick_label)
@@ -72,5 +90,5 @@ ax.spines['right'].set_linewidth(bwith)
 ax.yaxis.set_major_formatter(FormatStrFormatter('%.0f'))
 
 plt.legend(ncol=2, frameon=False, fontsize=30, loc='upper left')
-plt.savefig('DUR OFFICE.png', bbox_inches='tight', pad_inches=0.05, dpi=200)
+plt.savefig('Dur OFFICE.png', bbox_inches='tight', pad_inches=0.05, dpi=200)
 plt.show()
