42fda1c7d7e1e3fa9dc280fc075b34c3467235e4
KevinKang <e0751516@u.nus.edu>
2023-11-20 09:46:04 +0800

    exp

---
+++ results/steps.py
@@ -0,0 +1,91 @@
+import numpy as np
+import matplotlib
+from matplotlib import pyplot as plt
+#import seaborn as sns
+from matplotlib.ticker import FormatStrFormatter
+
+plt.rcParams['mathtext.fontset'] = 'stix'
+plt.rcParams['font.sans-serif'] = 'Times New Roman'
+plt.rcParams['axes.unicode_minus'] = False
+plt.rcParams['figure.figsize'] = (12, 8)
+plt.rcParams['font.size'] = 40
+plt.rcParams["font.weight"] = "bold"
+plt.rcParams["axes.labelweight"] = "bold"
+plt.rcParams["axes.linewidth"] = 2
+
+
+tick_label = ['3', '4', '5']
+
+
+#museum pre capture time
+# V2DN = [36.296,26.551,24.174]
+# CE_PG = [66.056,56.3,45.44]
+# MADDPG = [46.57,36.617,31.34]
+# VDN = [50.513,39.34,37.07]
+# DQN = [54.38,42.366,36]
+# DRL = [63.89,54.456,47.4]
+# PD_Fac = [52.32,43.2,32.04]
+# MASAC = [62.35,55.17,42.84]
+
+#office pre capture time
+# V2DN = [33.755,29.71,22.68]
+# CE_PG = [56.43,52.29,46.157]
+# MADDPG = [53.455,34.689,30.71]
+# VDN = [53,35.987,36.37]
+# DQN = [50.884,38.445,34.45]
+# DRL = [58.996,50.854,31.8]
+# PD_Fac = [49.72,40.53,34.92]
+# MASAC = [54.03,48.42,41.61]
+
+#museum during resiliency score
+# V2DN = [37.7,33.93,29]
+# CE_PG = [65.75,57.2,52.67]
+# MADDPG = [61.05,48.83,42.5]
+# VDN = [51.21,49.23,41.23]
+# DQN = [67.7,58.63,44.58]
+# DRL = [57.5,53.42,49]
+# PD_Fac = [56.7,43.255,38.28]
+# MASAC = [63.86,57.24,50.32]
+
+#office during resilency score
+V2DN = [35.77,32.74,25.968]
+CE_PG = [56.5,54.76,37.09]
+MADDPG = [55.029,38.33,34.079]
+VDN = [53.23,40.3,30.307]
+DQN = [70.06,44.84,37.3]
+DRL = [56.505,50,45.025]
+PD_Fac = [58.2,40.5,32]
+MASAC = [57.72,52.685,40.145]
+
+bar_width = 0.1
+bar_widthg = 0.105
+#tick_label = ['2', '3', '4']
+
+plt.bar(np.arange(3), V2DN, bar_width, align="center", label="V2DN", alpha=1,ec='k', ls='-', lw=3,color=(163/255, 6/255, 67/255))
+plt.bar(np.arange(3)+bar_widthg, CE_PG, bar_width, align="center", label="CE-PG", alpha=1,ec='k', ls='-', lw=3,color=(235/255, 96/255, 70/255))
+plt.bar(np.arange(3)+2*bar_widthg, MADDPG, bar_width, align="center", label="MADDPG", alpha=1,ec='k', ls='-', lw=3,color=(98/255, 190/255, 166/255))
+plt.bar(np.arange(3)+3*bar_widthg, VDN, bar_width, align="center", label="VDN", alpha=1,ec='k', ls='-', lw=3,color=(254/255, 251/255, 185/255))
+plt.bar(np.arange(3)+4*bar_widthg, DQN, bar_width, align="center", label="DQN", alpha=1,ec='k', ls='-', lw=3,color=(205/255, 234/255, 157/255))
+plt.bar(np.arange(3)+5*bar_widthg, DRL, bar_width, align="center", label="DRL", alpha=1,ec='k', ls='-', lw=3,color=(253/255, 186/255, 107/255))
+plt.bar(np.arange(3)+6*bar_widthg, PD_Fac, bar_width, align="center", label="PD-FAC", alpha=1,ec='k', ls='-', lw=3,color=(236/255, 217/255, 207/255))
+plt.bar(np.arange(3)+7*bar_widthg, MASAC, bar_width, align="center", label="MASAC", alpha=1,ec='k', ls='-', lw=3,color=(78/255, 146/255, 128/255))
+
+plt.ylim(20,120)
+plt.xlabel("Number of Robots ($N$)", weight='bold',fontsize=40)
+plt.ylabel("Capture time (Steps)", weight='bold',fontsize=40)
+plt.xticks(weight='bold')
+plt.yticks(weight='bold')
+plt.xticks(np.arange(3)+bar_widthg*2.5,tick_label)
+#plt.title("OFFICE During Execution rho = 0.2")
+
+bwith=2
+ax = plt.gca()
+ax.spines['bottom'].set_linewidth(bwith)
+ax.spines['left'].set_linewidth(bwith)
+ax.spines['top'].set_linewidth(bwith)
+ax.spines['right'].set_linewidth(bwith)
+ax.yaxis.set_major_formatter(FormatStrFormatter('%.0f'))
+
+plt.legend(ncol=2, frameon=False, fontsize=30, loc='upper left')
+plt.savefig('DUR OFFICE Steps.png', bbox_inches='tight', pad_inches=0.05, dpi=200)
+plt.show()
